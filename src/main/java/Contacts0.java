2b623c74430dbecfad64014a5e49f9c46e661a45
sarahchow03 <76396931+sarahchow03@users.noreply.github.com>
2024-08-30 17:52:08 +0800

    Improve Contacts0 class

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+    public static final int MAX_ENTITIES = 100;
+    public static final String SEPARATOR = "===================================================";
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
-        System.out.println("|| ===================================================");
-        System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
-        System.out.println("|| Welcome to Contacts!");
-        System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        displayWelcomeMessage();
+        String[][] list = new String[MAX_ENTITIES][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
@@ -140,10 +139,18 @@ public class Contacts0 {
                             + String.format("\tExample: %1$s", "help")));
                     break;
             }
-            for (String m : new String[]{feedback, "==================================================="}) {
+            for (String m : new String[]{feedback, SEPARATOR}) {
                 System.out.println("|| " + m);
             }
         }
+    }
+
+    private static void displayWelcomeMessage() {
+        System.out.println("|| ===================================================");
+        System.out.println("|| ===================================================");
+        System.out.println("|| Contacts - Version 0.0");
+        System.out.println("|| Welcome to Contacts!");
+        System.out.println("|| ===================================================");
     }
 
 }
